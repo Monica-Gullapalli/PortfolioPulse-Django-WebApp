6f03c3143b623799f9e2e76b2974575e11d9c4d6
Monica Gullapalli <monicagullapalli@gmail.com>
2024-04-10 23:51:39 -0600

    full project

--- financial_project/settings.py
+++ financial_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,18 +76,14 @@ WSGI_APPLICATION = "financial_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-    )
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
 }
+}
+
+
 
 
 # Password validation
